d0e283c5c4fa7a1bdc1f9e669d55ce1fd827fe4b
AdrianaMargiotta <adrianamargiotta@protonmail.com>
2020-11-18 13:22:44 +0100

    Schema logico e css

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// Creiamo una mini app di todolist con html+css+js in Vue. Features principali:
+// un input permette all’utente di scrivere una “cosa da fare”;
+// un bottone permette di aggiungere quella cosa alla lista di cose da fare in pagina, come promemoria;
+// una volta che l’utente ha ipoteticamente svolto l’attività, grazie ad un bottone (X) può eliminare quella determinata cosa dalla lista.
+
+// var app = new VTTCue({
+//     el: '#app',
+//     data: {
+
+//     },
+//     methodos: {
+//         //funzione per il bottone aggiungi
+
+//         //funzione per la X nel div list-item-delete
+
+//     }
+// });
