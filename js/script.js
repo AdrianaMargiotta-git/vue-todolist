5b37e09fa838704a3bb556ab82e1f84ee4c6e488
AdrianaMargiotta <adrianamargiotta@protonmail.com>
2020-11-18 16:53:33 +0100

    Latest code update

--- js/script.js
+++ js/script.js
@@ -2,11 +2,13 @@
 // un input permette all’utente di scrivere una “cosa da fare”;
 // un bottone permette di aggiungere quella cosa alla lista di cose da fare in pagina, come promemoria;
 // una volta che l’utente ha ipoteticamente svolto l’attività, grazie ad un bottone (X) può eliminare quella determinata cosa dalla lista.
+//Bonus: dò la possibilità anche di inserire la nuova “cosa da fare”, scritta nell’input anche con la pressione del tasto ‘enter’ da tastiera; (line 23html)
 
 var app = new Vue({
     el: '#app',
     data: {
         titolo: "TodoListVue by Adriana",
+        immagine: "img/vue-symbol.jpg",
         items: ["Fare la spesa", "Fare la lavatrice"],
         nuovoItem: ''
     },
@@ -25,9 +27,14 @@ var app = new Vue({
         },
 
         //funzione per la X nel div list-item-delete
-        removeItem: function(item){
-            this.items.splice(item, 1);
+        removeItem: function (item) {
+            this.items = this.items.filter((newItem) => newItem !== item);
         }
+
+        //non funziona come dovrebbe
+        // removeItem: function(item){
+        //     this.items.splice(item, 1);
+        // }
 
     }
 });
