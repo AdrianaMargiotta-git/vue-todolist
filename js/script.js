4da7e35560a8e1d5821c240aea2e9150dd927fce
AdrianaMargiotta <adrianamargiotta@protonmail.com>
2020-11-18 16:11:46 +0100

    Prima stesura del codice

--- js/script.js
+++ js/script.js
@@ -3,15 +3,31 @@
 // un bottone permette di aggiungere quella cosa alla lista di cose da fare in pagina, come promemoria;
 // una volta che l’utente ha ipoteticamente svolto l’attività, grazie ad un bottone (X) può eliminare quella determinata cosa dalla lista.
 
-// var app = new VTTCue({
-//     el: '#app',
-//     data: {
+var app = new Vue({
+    el: '#app',
+    data: {
+        titolo: "TodoListVue by Adriana",
+        items: ["Fare la spesa", "Fare la lavatrice"],
+        nuovoItem: ''
+    },
+    computed: {
+        reversedItems() {
+          return this.items.slice( 0 ).reverse();
+        },
+      },
+    methods: {
+        //funzione per il bottone aggiungi
+        aggiungiItem: function(){
+            if(this.nuovoItem.trim() !== ''){
+                this.items.push(this.nuovoItem);
+                this.nuovoItem = '';
+            };
+        },
 
-//     },
-//     methodos: {
-//         //funzione per il bottone aggiungi
+        //funzione per la X nel div list-item-delete
+        removeItem: function(item){
+            this.items.splice(item, 1);
+        }
 
-//         //funzione per la X nel div list-item-delete
-
-//     }
-// });
+    }
+});
